151ce09eac19ce45ed837b83dceb3a5c0e0cc75d
Isaac Zhou <isaac.zhou@appliedvalue.com>
2018-10-15 16:56:48 -0400

    Initial Commit

---
+++ MoMo.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MoMo.API.Models;
+
+namespace MoMo.API.Data
+{
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    public DbSet<Value> Values { get; set; }
+  }
+}
---
+++ MoMo.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace MoMo.API.Models
+{
+  public class Value
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
+}
